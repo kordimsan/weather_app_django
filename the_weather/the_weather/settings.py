1843bb7d3f0c5d5e41a3c3c1eeecbb0895304b70
Dmitry Korzunin <kordimsan@mail.ru>
2021-03-17 19:37:58 +0500

    hide secrets

--- the_weather/the_weather/settings.py
+++ the_weather/the_weather/settings.py
@@ -20,7 +20,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '_%8&aaez4#5%6)$$#rxdbyv5)(he7(2&@_d77_00tehjkrj$n2'
+SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
